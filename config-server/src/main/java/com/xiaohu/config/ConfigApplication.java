00db80ef0c921f9bda15dbd2c2a2ada6927d46bb
xiaohu <xiaohu.@520.com>
2021-01-15 21:37:59 +0800

    调整文件位置

--- config-server/src/main/java/com/xiaohu/config/ConfigApplication.java
+++ config-server/src/main/java/com/xiaohu/config/ConfigApplication.java
@@ -2,12 +2,14 @@ package com.xiaohu.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Author ChenHu
  * @Date 2021/1/15 - 20:51
  * @Describe
  **/
+@EnableConfigServer
 @SpringBootApplication
 public class ConfigApplication {
 
