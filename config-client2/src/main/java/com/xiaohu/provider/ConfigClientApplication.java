3dd4b6c6122ac3cfeb6948eb5b42745b1eb59512
xiaohu <xiaohu.@520.com>
2021-01-16 15:26:21 +0800

    提交

--- config-client2/src/main/java/com/xiaohu/provider/ConfigClientApplication.java
+++ config-client2/src/main/java/com/xiaohu/provider/ConfigClientApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 /**
  * @Author ChenHu
  * @Date 2021/1/16 - 1:41
- * @Description 这个项目是失败了，我本以为，config-server服务端，包含了客户端的jar，可以直接引用服务端的依赖，就能完成客户端的功能
+ * @Description 这个项目是失败了。我本以为，config-server服务端，包含了客户端的jar，可以直接引用服务端的依赖，就能完成客户端的功能
  * 后来试过多次，包括引入spring-cloud-context，依然无法使bootstrap.yml文件生效。自然无法加载到配置中心的配置
  * @Return
  **/
