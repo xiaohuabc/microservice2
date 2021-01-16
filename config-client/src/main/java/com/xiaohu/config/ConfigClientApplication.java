8dc7b7f10fe2f50019e03634999c418e56b7da5b
xiaohu <xiaohu.@520.com>
2021-01-16 14:56:58 +0800

    提交

--- config-client/src/main/java/com/xiaohu/config/ConfigClientApplication.java
+++ config-client/src/main/java/com/xiaohu/config/ConfigClientApplication.java
@@ -3,19 +3,17 @@ package com.xiaohu.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @Author ChenHu
- * @Date 2021/1/15 - 20:51
+ * @Date 2021/1/15 - 23:05
  * @Describe
  **/
-@EnableConfigServer
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigApplication {
+public class ConfigClientApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class,args);
+        SpringApplication.run(ConfigClientApplication.class,args);
     }
 }
